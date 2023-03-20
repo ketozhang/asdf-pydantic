3f6e695d02bc22667c36665d987b6e0a9061e804
Keto Zhang <keto.zhang@gmail.com>
2023-03-19 20:15:17 -0700

    Fix lint

--- asdf_pydantic/model.py
+++ asdf_pydantic/model.py
@@ -1,18 +1,9 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 
-from asdf import AsdfFile
 from pydantic import BaseModel
 
 
 class AsdfBaseModel(ABC, BaseModel):
-    # @abstractmethod
-    # def to_yaml(self):
-    #     pass
-
-    # @abstractmethod
-    # def from_yaml(self):
-    #     pass
-
     def asdf_yaml_tree(self) -> dict:
         d = {}
         for field_key, v in self.__dict__.items():

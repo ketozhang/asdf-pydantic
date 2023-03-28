ce9bfb87a40ac9d9869170228b90f11685d8cb76
Keto D. Zhang <ketoz@ipac.caltech.edu>
2023-03-27 22:46:53 -0700

    Remove AsdfTimeEntry in extension

--- asdf_pydantic/examples/extensions.py
+++ asdf_pydantic/examples/extensions.py
@@ -3,16 +3,15 @@ from asdf.extension import Extension
 from asdf_pydantic.converter import AsdfPydanticConverter
 from asdf_pydantic.examples.shapes import AsdfRectangle
 from asdf_pydantic.examples.tree import AsdfNode
-from asdf_pydantic.examples.with_units import AsdfTimeEntry
 
-AsdfPydanticConverter.add_models(AsdfRectangle, AsdfNode, AsdfTimeEntry)
+AsdfPydanticConverter.add_models(AsdfRectangle, AsdfNode)
+
 
 class ExampleExtension(Extension):
     extension_uri = "asdf://asdf-pydantic/examples/extensions/examples-1.0.0"
-    converters = [
-        AsdfPydanticConverter()
-    ]  # type: ignore
+    converters = [AsdfPydanticConverter()]  # type: ignore
     tags = [*AsdfPydanticConverter().tags]  # type: ignore
+
 
 def get_extensions() -> list[Extension]:
     return [ExampleExtension()]

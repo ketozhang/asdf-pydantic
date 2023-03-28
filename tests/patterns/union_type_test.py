065b3f71d900bfe5c7cc09f23cad05c2a76ba425
Keto D. Zhang <ketoz@ipac.caltech.edu>
2023-03-27 22:26:05 -0700

    Update union test checking identity conversion of standard types

---
+++ tests/patterns/union_type_test.py
@@ -0,0 +1,108 @@
+from datetime import datetime
+from typing import Any, Optional
+
+import asdf
+import pytest
+from asdf.extension import Extension
+from astropy.time import Time
+
+from asdf_pydantic import AsdfPydanticConverter, AsdfPydanticModel
+
+
+class UnionObject(AsdfPydanticModel):
+    _tag = "asdf://asdf-pydantic/examples/tags/unionobject-1.0.0"
+
+    # Order of type matters
+    int_or_str: int | str = 0
+    time: Time | datetime = datetime(2023, 1, 1)
+    anything: Optional[Any] = None
+
+
+def setup_module():
+    AsdfPydanticConverter.add_models(UnionObject)
+
+    class TestExtension(Extension):
+        extension_uri = "asdf://asdf-pydantic/examples/extensions/test-1.0.0"
+
+        converters = [AsdfPydanticConverter()]  # type: ignore
+        tags = [*AsdfPydanticConverter().tags]  # type: ignore
+
+    asdf.get_config().add_extension(TestExtension())
+
+
+def test_str_converts_to_str(tmp_path):
+    af = asdf.AsdfFile(
+        {
+            "obj": UnionObject(
+                int_or_str="test",
+            )
+        }
+    )
+    af.write_to(tmp_path / "test.asdf")
+
+    with asdf.open(tmp_path / "test.asdf") as af:
+        assert isinstance(af["obj"].int_or_str, str)
+
+
+def test_integer_converts_to_int(tmp_path):
+    asdf.AsdfFile(
+        {
+            "obj": UnionObject(
+                int_or_str=0,
+                time=Time("2023-01-01T00:00:00"),
+            )
+        }
+    ).write_to(tmp_path / "test.asdf")
+
+    with asdf.open(tmp_path / "test.asdf") as af:
+        assert isinstance(af["obj"].int_or_str, int)
+
+
+def test_datetime_converts_to_datetime(tmp_path):
+    af = asdf.AsdfFile(
+        {
+            "obj": UnionObject(
+                time=datetime(2023, 1, 1, 1, 0, 0),
+            )
+        }
+    )
+    af.write_to(tmp_path / "test.asdf")
+
+    with asdf.open(tmp_path / "test.asdf") as af:
+        assert isinstance(af["obj"].time, datetime)
+
+
+def test_Time_converts_to_Time(tmp_path):
+    af = asdf.AsdfFile(
+        {
+            "obj": UnionObject(
+                time=Time("2023-01-01T01:00:00"),
+            )
+        }
+    )
+    af.write_to(tmp_path / "test.asdf")
+
+    with asdf.open(tmp_path / "test.asdf") as af:
+        assert isinstance(af["obj"].time, Time)
+
+
+@pytest.mark.parametrize(
+    "value",
+    [
+        0,
+        1.0,
+        "asdf",
+        dict(key="value"),
+        # (1, 2, 3),  # Tuples are returned as list in ASDF
+        [1, 2, 3],
+        {1, 2, 3},
+        Time("2023-01-01T01:00:00"),
+        datetime(2023, 1, 1, 1, 0, 0),
+    ],
+)
+def test_Any_converts_back_to_itself(value, tmp_path):
+    af = asdf.AsdfFile({"obj": UnionObject(anything=value)})
+    af.write_to(tmp_path / "test.asdf")
+
+    with asdf.open(tmp_path / "test.asdf") as af:
+        assert isinstance(af["obj"].anything, type(value))
